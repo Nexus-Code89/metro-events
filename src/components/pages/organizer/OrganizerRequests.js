7c121c3992878196f9cd1c84eac2138bb4bb1a62
John Ivan Puayap <johnivanpuayap@gmail.com>
2024-03-26 13:23:43 +0800

    Add Nav Component for Organizers

--- src/components/pages/organizer/OrganizerRequests.js
+++ src/components/pages/organizer/OrganizerRequests.js
@@ -1,20 +1,12 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import OrganizerNav from './OrganizerNav';
 
 const OrganizerRequests = () => {
   return (
     <section>
       <div className="menu-header">
         <h2>Organizer Requests</h2>
-        <nav>
-          <ul>
-            <li><Link to="/organizer-dashboard">Home</Link></li>
-            <li><Link to="/organizer-profile">Profile</Link></li>
-            <li><Link to="/organizer-events">Events</Link></li>
-            <li><Link to="/organizer-requests">Requests</Link></li>
-            <li><Link to="/logout">Logout</Link></li>
-          </ul>
-        </nav>
+        <OrganizerNav />
       </div>
       {/* Your dashboard content goes here */}
     </section>
